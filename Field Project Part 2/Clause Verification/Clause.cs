b376e366d3f187b19b6271fba3db94409ab82d82
Landry Droupy <Landry.J.Droupy-1@ou.edu>
2022-10-20 16:51:30 -0500

    Creating the Clause class

---
+++ Field Project Part 2/Clause Verification/Clause.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clause_Verification
+{
+    internal class Clause
+    {
+      public string  clauseID  {get; set;}
+      public string  P_C       {get; set;}
+      public string  IBR	   {get; set;}
+      public string  FILL_IN   {get; set;}
+      public string  MONEY     {get; set;}
+      public string  UCF	   {get; set;}
+      public string  FP_SUP	   {get; set;}
+      public string  CR_SUP	   {get; set;}
+      public string  FP_R_D	   {get; set;}
+      public string  CR_R_D	   {get; set;}
+      public string  FP_SVC	   {get; set;}
+      public string  CR_SVC	   {get; set;}
+      public string  FP_CON	   {get; set;}
+      public string  CR_CON	   {get; set;}
+      public string  T_M_LH	   {get; set;}
+      public string  LMV	   {get; set;}
+      public string  COM_SVC    {get; set;}
+      public string  DDR	   {get; set;}
+      public string  A_E	   {get; set;}
+      public string  FAC	   {get; set;}
+      public string  IND_DEL   {get; set;}
+      public string  TRN	   {get; set;}
+      public string  UTL_SVC    {get; set;}
+
+        //We made this variable an integer, we weren't sure if we would need it or not
+        public int Dep_P_C { get; set; }
+
+    }
+
+   
+
+
+
+
+
+
+
+}
